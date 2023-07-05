6cfedf7a54b1cfe5387c075ce62c905f6ca3c5fe
kazuya.zama <zama@restarts.co.jp>
2023-07-05 22:45:33 +0900

    fix:eslintの再設定

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,14 +2,27 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-	fontFamily:{
-		"roboto":["Roboto Flex Variable", "sans-serif"],
-		"montserrat":["montserrat Variable","sans-serif"]
-	},
+    fontFamily: {
+      roboto: ["Roboto Flex Variable", "sans-serif"],
+      montserrat: ["montserrat Variable", "sans-serif"],
+    },
     extend: {
-      backgroundImage:{
-        "access-image":"url('src/assets/Frame 116.png')"
-      }
+      backgroundImage: {
+        "access-image": "url('src/assets/Frame 116.png')",
+      },
+      keyframes: {
+        pathmove: {
+          "0%": { height: 0, top: 0, opacity: 0 },
+          "30%":{
+            height:"40px",
+            opacity: 1
+          },
+          "100%":{height:0,top:"60px",opacity: 0},
+        },
+      },
+      animation: {
+        "scroll-down": "pathmove 1.4s ease-in-out infinite",
+      },
     },
   },
   plugins: [require("daisyui"), require("@tailwindcss/typography")],
