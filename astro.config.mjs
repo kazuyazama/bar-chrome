e6bbf0890ea7585042b9bdcc583855288a0c8613
kazuya.zama <zama@restarts.co.jp>
2023-07-07 16:23:27 +0900

    fix:astroImageToolsの記載削除

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import { astroImageTools } from "astro-imagetools";
 import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), image(), astroImageTools],
+  integrations: [tailwind(), react(), image()],
   // experimental: {
   //   assets: true
   // }
