f9c89cb5c6d5a47fcea2a9155c1af18add4a699c
kazuya.zama <zama@restarts.co.jp>
2023-07-07 16:17:18 +0900

    refactor:画像の遅延読み込みの調整

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
+import { astroImageTools } from "astro-imagetools";
 import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), image()],
+  integrations: [tailwind(), react(), image(), astroImageTools],
   // experimental: {
   //   assets: true
   // }
